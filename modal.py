95c0c5842e68e29890facf0c2258abdc037887cf
TeslaSSH <146672794+TeslaSSH@users.noreply.github.com>
2024-01-13 16:53:02 +0300

    Update modal.py

--- modal.py
+++ modal.py
@@ -4,20 +4,20 @@ from datetime import datetime, timedelta
 
 # Read the bot token from the file
 #with open('bot_token.txt', 'r') as file:
-   # bot_token = file.read().strip()
+  #  bot_token = file.read().strip()
 
 # Initialize the bot with the token
 bot = telepot.Bot('6892057864:AAErqK-yT3DVE-AcRGJqZP9Mj6fPzhrP-3M')
 
-def add_user(username, password, days, user_info):
+def add_user(username, password, days):
     current_date = datetime.now()
     expiration_date = current_date + timedelta(days=int(days))
     expiration_date_str = expiration_date.strftime('%Y-%m-%d')
 
     # Check if the user already exists
     existing_users = subprocess.check_output(['cat', '/etc/passwd']).decode('utf-8')
-    if f'{username}:' in existing_users and user_info.lower() not in existing_users.lower():
-        return f"User {username} already exists with a different info."
+    if f'{username}:' in existing_users:
+        return f"User {username} already exists."
 
     # Generate hashed password
     osl_version = subprocess.check_output(['openssl', 'version']).decode('utf-8')
@@ -27,7 +27,7 @@ def add_user(username, password, days, user_info):
 
     # Create user
     try:
-        subprocess.run(['sudo', 'useradd', '-M', '-s', '/bin/false', '-e', expiration_date_str, '-K', f'PASS_MAX_DAYS={days}', '-p', hashed_password, '-c', user_info, username], check=True)
+        subprocess.run(['sudo', 'useradd', '-M', '-s', '/bin/false', '-e', expiration_date_str, '-K', f'PASS_MAX_DAYS={days}', '-p', hashed_password, '-c', f'"{password}",{username}', username], check=True)
         return f"User {username} added successfully!"
     except subprocess.CalledProcessError as e:
         return f"Failed to add user {username}. Error: {e}"
@@ -40,11 +40,11 @@ def handle(msg):
 
         if command.startswith('/add'):
             try:
-                _, username, password, days, user_info = command.split()
-                response = add_user(username, password, days, user_info)
+                _, username, password, days = command.split()
+                response = add_user(username, password, days)
                 bot.sendMessage(chat_id, response)
             except ValueError:
-                bot.sendMessage(chat_id, "Invalid command format. Use /add username password days user_info")
+                bot.sendMessage(chat_id, "Invalid command format. Use /add username password days")
 
 # Set the command handler
 bot.message_loop(handle)

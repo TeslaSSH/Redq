2827f207264980a9e3c2ac6a8465ed2f8178a0c3
TeslaSSH <146672794+TeslaSSH@users.noreply.github.com>
2024-01-13 08:37:07 +0300

    Update modal.py

--- modal.py
+++ modal.py
@@ -9,9 +9,24 @@ def handle(msg):
 
     if content_type == 'text':
         command = msg['text']
-        if command.startswith('/adduser'):
+
+        if command == '/start':
+            welcome_message = ("Hello, welcome to Tesla SSH scripts manager. "
+                               "You can use me to create more users for your server!\n"
+                               "Press /start to reload the bot\n"
+                               "Press /help to see the usage guide\n"
+                               "Press /add to add user")
+            bot.sendMessage(chat_id, welcome_message)
+
+        elif command == '/help':
+            help_message = "Usage guide:\n/add [username] - Add a new user"
+            bot.sendMessage(chat_id, help_message)
+
+        elif command.startswith('/add'):
             username = command.split()[1]
             subprocess.call(['sudo', 'adduser', username])
+            success_message = f"User {username} added successfully!"
+            bot.sendMessage(chat_id, success_message)
 
 # Set the command handler
 bot.message_loop(handle)

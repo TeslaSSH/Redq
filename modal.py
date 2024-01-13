3de9840e71e31793bbadd346aa6d5917b0ff4621
TeslaSSH <146672794+TeslaSSH@users.noreply.github.com>
2024-01-13 08:14:15 +0300

    Create modal.py

---
+++ modal.py
@@ -0,0 +1,21 @@
+import telepot
+import subprocess
+
+# Replace 'YOUR_BOT_TOKEN' with your Telegram bot token
+bot = telepot.Bot('YOUR_BOT_TOKEN')
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    if content_type == 'text':
+        command = msg['text']
+        if command.startswith('/adduser'):
+            username = command.split()[1]
+            subprocess.call(['sudo', 'adduser', username])
+
+# Set the command handler
+bot.message_loop(handle)
+
+# Keep the program running
+while True:
+    pass

